f05766618225118a0e8fda2c87909b56b54ef8d5
callmeVineeth <vineethpv024@gmail.com>
2024-03-11 20:48:15 +0530

    Author details changed

--- src/Components/PostAuthor.jsx
+++ src/Components/PostAuthor.jsx
@@ -4,15 +4,15 @@ import { Link } from 'react-router-dom'
 function PostAuthor({avathar, authorName, upload_time}) {
   return (
     <Link to={'/posts/users/'}  className=' text-decoration-none text-black'>
-      <section className='d-flex flex-row align-content-center
+      <section className='d-flex flex-row gap-2 align-content-center
      justify-content-start'>
           <div className=' w-25'>
-              <img src={avathar} alt='AuthorImage' className='' style={{ width: '100%'}}/>
+              <img src={avathar} alt='AuthorImage' className=' rounded-4' style={{ width: '100%'}}/>
           
           </div>
-          <section className=' w-auto bg-info-subtle d-flex '>
-            <strong className=' text-opacity-75'> {authorName}</strong> <br/>
-            <small className=' opacity-50'>{upload_time}</small>
+          <section className=' w-auto d-flex flex-column mt-1 '>
+            <strong className=' text-opacity-75'> {authorName}</strong> 
+            <small className='  opacity-50'>{upload_time}</small>
           </section>
       </section>
     </Link>
