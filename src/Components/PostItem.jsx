7afe0aedc9c85d7eacb11fe94cd78d9c43eed4f7
callmeVineeth <vineethpv024@gmail.com>
2024-03-11 20:52:10 +0530

    posts  page updated

--- src/Components/PostItem.jsx
+++ src/Components/PostItem.jsx
@@ -5,8 +5,8 @@ import PostAuthor from './PostAuthor'
 function PostItem({title, description, thumbnail,avathar, authorName, upload_time}) {
   return (
     
-    <Card style={{ width: '20rem' }} className=' rounded-4'>
-      <div className=' p-2'>
+    <Card style={{ width: '20rem' }} className=' p-2 mb-0 rounded-4'>
+      <div className=' '>
       <Card.Img variant="top" src={thumbnail} alt='thumbnail' className=' rounded-4' />
        <Card.Body>
         <Card.Title>{title}</Card.Title>
