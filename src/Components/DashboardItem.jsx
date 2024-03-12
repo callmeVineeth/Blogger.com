b7881a603611ea454ed26adf0bbc4e0d65702353
callmeVineeth <vineethpv024@gmail.com>
2024-03-12 21:08:54 +0530

    dashboard updated

---
+++ src/Components/DashboardItem.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Row,Col } from 'react-bootstrap'
+
+function DashboardItem({postImg, details}) {
+  return (
+    <div>
+        <Row  className=' bg-info p-2 m-2 d-flex flex-row justify-content-between'>
+           <Col>
+            <img src={postImg}  className=' w-25' alt='postsImage' />
+            <p>{details}</p>
+           </Col>
+
+           <Col>
+            <button className=' m-1'>Edit</button>
+            <button className=' m-1'>Delete</button>
+           </Col>
+        </Row>
+      
+    </div>
+  )
+}
+
+export default DashboardItem
--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import DummyData from '../Components/Data';
+import DashboardItem from '../Components/DashboardItem';
 
 function Dashboard() {
   // Make sure DummyData is an array before passing to useState
@@ -9,19 +10,13 @@ function Dashboard() {
 
   return (
     <div className=' h-50 w-75'>
-      <h1>Dashboard</h1>
+      <h1>Dashboard</h1> 
+
+     
       {userData.map((user, id) => (
-        <div className='bg-info' key={id}>
-          <div xs={3}>
-            <img src={user.image} alt={`Avatar of ${user.name}`} />
-          </div>
-          <div xs={9}>
-            <h2>{user.name}</h2>
-            <p>Posts: {user.posts}</p>
-            <p>Joined: {user.Joined}</p>
-          </div>
-        </div>
+      <DashboardItem key={id} postImg={user.image} details={user.Joined}/> 
       ))}
+
     </div>
   );
 }
